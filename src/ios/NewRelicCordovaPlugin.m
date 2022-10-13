09c33bc978cec535a6b43cf471b2b93863705a84
ywang <ywang@newrelic.com>
2022-10-13 17:11:00 -0400

    [NR-56625] Update ios release notes link

--- src/ios/NewRelicCordovaPlugin.m
+++ src/ios/NewRelicCordovaPlugin.m
@@ -1,7 +1,7 @@
 //  New Relic for Mobile -- iOS edition
 //
 //  See:
-//    https://docs.newrelic.com/docs/releases/ios for release notes
+//    https://docs.newrelic.com/docs/release-notes/mobile-release-notes/xcframework-release-notes for release notes
 //
 //  Copyright (c) 2017 - Present New Relic. All rights reserved.
 //  See https://docs.newrelic.com/docs/licenses/ios-agent-licenses for license details
