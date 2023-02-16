8623799ea40e2fe584c9c382142d153747531da2
Kenny <kthai@newrelic.com>
2023-02-15 17:05:50 -0800

    fix: Prune cyclical structures in stringify

--- www/js/newrelic.js
+++ www/js/newrelic.js
@@ -108,7 +108,8 @@ var NewRelic = {
     },
 
     sendConsole(type, args) {
-        const argsStr = JSON.stringify(args);
+
+        const argsStr = JSON.stringify(args, getCircularReplacer());
         this.send('MobileJSConsole', { consoleType: type, args: argsStr });
     },
 
@@ -496,5 +497,22 @@ const NewRelicEvent = class CustomEvent {
     }
 };
 
+/**
+ * Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Cyclic_object_value
+ * Any copyright is dedicated to the Public Domain: https://creativecommons.org/publicdomain/zero/1.0/
+ */
+const getCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (key, value) => {
+      if (typeof value === "object" && value !== null) {
+        if (seen.has(value)) {
+          return;
+        }
+        seen.add(value);
+      }
+      return value;
+    };
+};
+  
 
 module.exports = NewRelic;
